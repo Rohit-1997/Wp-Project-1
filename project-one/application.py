f03d943b47987c902a7998e9a465a4e24dd9bdba
KaloriRohit <kalorirohit@msitprogram.net>
2020-04-17 01:45:45 +0530

    User table created successfully

--- project-one/application.py
+++ project-one/application.py
@@ -1,5 +1,7 @@
 # importing the modules
+import os
 from flask import Flask, render_template, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
 # creating an instance of Flask
@@ -7,6 +9,11 @@ app = Flask(__name__)
 
 # setting up the secret key for the application
 app.config['SECRET_KEY'] = 'f9a1520561f1faf67f36a3a620a45e80'
+
+# setting up the database and database instance
+app.config["SQLAlchemy_DATABASE_URI"] = 'postgres://rvfihnnxofkits:38a240139b9c256dcb9b803266fd4576da0379815cc043c4b28fedc99503c709@ec2-34-197-212-240.compute-1.amazonaws.com:5432/d6jd4cr061n7n6'
+# app.config["SQLAlchemy_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
 
 # The home page function
 @app.route("/")
@@ -38,6 +45,8 @@ def login():
         else:
             flash('login not succecssful', 'danger')
     return render_template('login.html', title='Login', login_form=login_form)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
